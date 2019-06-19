b70d0368eed6e35a64267ebbd9fd965d768fbf4e
Max Alva <alva85@gmail.com>
2019-06-18 23:38:24 -0500

    Diseño complejo

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
-import 'package:designs_flutter/src/pages/scroll_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
+import 'package:designs_flutter/src/pages/buttons_page.dart';
+import 'package:designs_flutter/src/pages/scroll_page.dart';
 import 'package:designs_flutter/src/pages/basic_page.dart';
 
 void main() => runApp(MyApp());
@@ -8,13 +10,17 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light
+        .copyWith(statusBarColor: Colors.transparent));
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Diseños',
-      initialRoute: 'scroll',
+      initialRoute: 'buttons',
       routes: {
         'basic': (context) => BasicPage(),
-        'scroll': (context) => ScrollPage()
+        'scroll': (context) => ScrollPage(),
+        'buttons': (context) => ButtonsPage()
       },
     );
   }
---
+++ lib/src/pages/buttons_page.dart
@@ -0,0 +1,165 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+class ButtonsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Stack(
+          children: <Widget>[
+            _fondoApp(),
+            SingleChildScrollView(
+              child: Column(
+                children: <Widget>[_titulos(), _botonesRedondeados()],
+              ),
+            )
+          ],
+        ),
+        bottomNavigationBar: _bottomNavigationBar(context));
+  }
+
+  Widget _fondoApp() {
+    final _gradient = Container(
+      width: double.infinity,
+      height: double.infinity,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: FractionalOffset(0.0, 0.6),
+          end: FractionalOffset(0.0, 1.0),
+          colors: [
+            Color.fromRGBO(52, 54, 101, 1.0),
+            Color.fromRGBO(35, 37, 57, 1.0),
+          ],
+        ),
+      ),
+    );
+
+    final _cajaRosada = Transform.rotate(
+      angle: -pi / 5.0,
+      child: Container(
+        height: 360.0,
+        width: 360.0,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(80.0),
+            gradient: LinearGradient(colors: [
+              Color.fromRGBO(236, 98, 188, 1.0),
+              Color.fromRGBO(241, 142, 172, 1.0),
+            ])),
+      ),
+    );
+
+    return Stack(
+      children: <Widget>[
+        _gradient,
+        Positioned(
+          top: -100,
+          child: _cajaRosada,
+        ),
+      ],
+    );
+  }
+
+  Widget _titulos() {
+    return SafeArea(
+      child: Container(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              'Lorem ipsum dolor',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10.0),
+            Text(
+              'Proin vitae risus tempor, semper ipsum ac, suscipit lacus.',
+              style: TextStyle(color: Colors.white, fontSize: 18.0),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _bottomNavigationBar(BuildContext context) {
+    return Theme(
+      data: Theme.of(context).copyWith(
+        canvasColor: Color.fromRGBO(55, 57, 84, 1.0),
+        primaryColor: Colors.pinkAccent,
+        textTheme: Theme.of(context).textTheme.copyWith(
+            caption: TextStyle(color: Color.fromRGBO(116, 117, 152, 1.0))),
+      ),
+      child: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_today, size: 30.0),
+            title: Container(),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.bubble_chart, size: 30.0),
+            title: Container(),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.supervised_user_circle, size: 30.0),
+            title: Container(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _botonesRedondeados() {
+    return Table(
+      children: [
+        TableRow(children: [
+          _boton(Colors.blue, Icons.border_all, 'General'),
+          _boton(Colors.purpleAccent, Icons.directions_bus, 'Bus'),
+        ]),
+        TableRow(children: [
+          _boton(Colors.redAccent, Icons.disc_full, 'General'),
+          _boton(Colors.lime, Icons.domain, 'General'),
+        ]),
+        TableRow(children: [
+          _boton(Colors.amber, Icons.do_not_disturb, 'General'),
+          _boton(Colors.orange, Icons.drive_eta, 'General'),
+        ]),
+        TableRow(children: [
+          _boton(Colors.blue, Icons.border_all, 'General'),
+          _boton(Colors.blue, Icons.border_all, 'General'),
+        ])
+      ],
+    );
+  }
+
+  Widget _boton(Color color, IconData icon, String texto) {
+    return ClipRect(
+      child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 0.0),
+        child: Container(
+          height: 180.0,
+          margin: EdgeInsets.all(15.0),
+          decoration: BoxDecoration(
+            color: Color.fromRGBO(62, 66, 107, 0.7),
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              CircleAvatar(
+                backgroundColor: color,
+                radius: 35.0,
+                child: Icon(icon, color: Colors.white, size: 30.0),
+              ),
+              Text(texto, style: TextStyle(color: color))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
